22c5fe4f352613d79be3f3e7b361f576464fd988
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-08-09 11:03:44 -0300

    Atualização

---
+++ Aula02/src/LPII_Aula2_2/Conta.java
@@ -0,0 +1,10 @@
+
+package LPII_Aula2_2;
+
+public class Conta {
+    protected static String agencia = "0148-8";
+    
+    public Conta(){
+        
+    }
+}
---
+++ Aula02/src/LPII_Aula2_2/Corrente.java
@@ -0,0 +1,13 @@
+
+package LPII_Aula2_2;
+
+public class Corrente extends Conta{
+    private static String numeroConta;
+    
+    public static void main(String[] args) {
+        numeroConta = "19584-8";
+        System.out.println("Os Dados bancarios são: ");
+        System.out.println("Ag:" + agencia + " Num:" + numeroConta);
+        
+    }
+}
