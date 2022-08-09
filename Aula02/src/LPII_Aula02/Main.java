e01be01fafb042f364061aeebf85dac08f7f3e79
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-08-09 08:16:05 -0300

    Criação do repositorio e Adição da aula 2

---
+++ Aula02/src/LPII_Aula02/Main.java
@@ -0,0 +1,12 @@
+package LPII_Aula02;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.mostrarNome();
+        
+    }
+    
+}
---
+++ Aula02/src/LPII_Aula02/Pessoa.java
@@ -0,0 +1,13 @@
+package LPII_Aula02;
+
+public class Pessoa {
+    public String nome;
+    
+    public Pessoa(){
+        nome = "Luquinha";
+    }
+    public void mostrarNome(){
+        System.out.println("O nome é: " + nome);
+    }
+    
+}
